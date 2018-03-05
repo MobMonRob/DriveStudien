1535355abe6cffdb937ffea224caa7799c3aa088
BrennerX <BrennerX96@gmail.com>
2018-03-05 10:15:02 +0100

    Add converter for light button

---
+++ src/DrivePlus.Client/View/Converter/LightButtonTextConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace DrivePlus.Client.View.Converter
+{
+    public class LightButtonTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var isLightOn = value != null && (bool)value;
+            return isLightOn ? "Licht ist an" : "Licht ist aus";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DrivePlus.Client/View/Converter/LightButtonToolTipConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace DrivePlus.Client.View.Converter
+{
+    public class LightButtonToolTipConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var isLightOn = value != null && (bool)value;
+            return isLightOn ? "Licht ausschalten" : "Licht anschalten";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
