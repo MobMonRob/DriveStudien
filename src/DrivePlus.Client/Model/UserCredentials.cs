5f648774af5a937a564a1120275713245e386961
BrennerX <BrennerX96@gmail.com>
2018-05-07 13:31:53 +0200

    Add model class for user credentials

---
+++ src/DrivePlus.Client/Model/UserCredentials.cs
@@ -0,0 +1,14 @@
+﻿namespace DrivePlus.Client.Model
+{
+    public class UserCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public UserCredentials(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
