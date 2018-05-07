37fbae52386d31557ebcd786358ac6bb0956fb6a
BrennerX <BrennerX96@gmail.com>
2018-05-07 22:26:14 +0200

    Add CameraAdapter property

--- src/DrivePlus.Client/Model/Camera.cs
+++ src/DrivePlus.Client/Model/Camera.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using DrivePlus.Contracts;
 
 namespace DrivePlus.Client.Model
 {
     public class Camera
     {
         public CameraParameter Parameter { get; set; }
+        public ICameraAdapter CameraAdapter { get; set; }
 
         public Camera(Uri cameraUri, string username, string password)
         {
             Parameter = new CameraParameter(cameraUri, username, password);
+            CameraAdapter = new CameraAdapter.CameraAdapter(cameraUri);
         }
     }
 }
