a9474a48f6fb30db28122d08222135cadd800d56
BrennerX <BrennerX96@gmail.com>
2018-05-07 14:19:40 +0200

    Add interface ICameraAdapter

---
+++ src/DrivePlus.Contracts/ICameraAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace DrivePlus.Contracts
+{
+    public interface ICameraAdapter
+    {
+        void Login(Uri camerUri, UserCredentials userCredentials);
+
+        Grid GetCamerGrid();
+    }
+}
