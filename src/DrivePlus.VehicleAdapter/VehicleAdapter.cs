35dcc29db2f7997c7c0425db85bbc2ee6ff0feb4
BrennerX <BrennerX96@gmail.com>
2018-05-09 01:04:39 +0200

    Add VehicleAdapter project with partial implementation of IVehicleAdapter

---
+++ src/DrivePlus.VehicleAdapter/VehicleAdapter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using DrivePlus.Contracts;
+using System.Net;
+
+namespace DrivePlus.VehicleAdapter
+{
+    public class VehicleAdapter : IVehicleAdapter
+    {
+        private readonly Uri _vehicleUri;
+
+        public VehicleAdapter()
+        {
+            _vehicleUri = new Uri("http://192.168.2.100:80");
+        }
+
+        public void SendCommand(VehicleCommand command, int value = 0)
+        {
+            WebRequest request;
+            
+            switch (command)
+            {
+                case (VehicleCommand.Stop):
+                    request = WebRequest.Create(_vehicleUri + "stop");
+                    break;
+                case (VehicleCommand.Forward):
+                    request = WebRequest.Create(_vehicleUri + "forward");
+                    break;
+                case (VehicleCommand.Backward):
+                    request = WebRequest.Create(_vehicleUri + "backward");
+                    break;
+                case (VehicleCommand.Left):
+                    request = WebRequest.Create(_vehicleUri + "left");
+                    break;
+                case (VehicleCommand.Right):
+                    request = WebRequest.Create(_vehicleUri + "right");
+                    break;
+                case (VehicleCommand.SwitchLightOn):
+                    request = WebRequest.Create(_vehicleUri + "light_on");
+                    break;
+                case (VehicleCommand.SwitchLightOff):
+                    request = WebRequest.Create(_vehicleUri + "light_off");
+                    break;
+                case (VehicleCommand.SetSpeed):
+                    request = WebRequest.Create(_vehicleUri + "set_speed=" + value);
+                    break;
+                default:
+                    return;
+            }
+
+            request.Timeout = 30;
+
+            try
+            {
+                request.GetResponse();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Command sent");
+            }
+        }
+
+        public string GetDistance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
