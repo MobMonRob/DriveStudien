6eda4ce3a219e1c0206b60f7fab16b2ab8c18ad4
BrennerX <BrennerX96@gmail.com>
2018-05-12 17:32:49 +0200

    Dynamic tool tip

---
+++ src/DrivePlus.Client/View/Converter/NightVisionButtonToolTipConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace DrivePlus.Client.View.Converter
+{
+    public class NightVisionButtonToolTipConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var isLightOn = value != null && (bool)value;
+            return isLightOn ? "Nachtsicht ausschalten" : "Nachsicht einschalten";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
